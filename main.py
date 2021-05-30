60aadf9542b1c73677e1da6744b0bc034373fbfb
AryakRoy <aryakroy63@gmail.com>
2021-05-30 19:30:49 +0530

    Sentiment Analysis Setup

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ from plotly import graph_objs as go
 from urllib import request
 import pandas as pd
 import base64
-
+from traverse import Traverse
+from Stock_Sentiment_Analysis import Sentiment_Analyzer
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
+    page_title="Stock Analytics App",
     page_icon="📈",
     layout="wide",
 )
+
+traverser = Traverse()
+sentiment_analyzer = Sentiment_Analyzer()
 
 @st.cache
 def load_stock_data(ticker):
@@ -28,6 +32,13 @@ def load_stock_list():
     df = data[0]
     return df
 
+st.title("Stock Analysis and Prediction")
+st.markdown("""
+This app retrieves the list of the **S&P 500** (from Wikepedia) and its corresponding **stock closing price** (year-to-date).
+* **Python Libraries :** base64, pandas, numpy, streamlit, yfinance, matplotlib, plotly, sklearn, keras
+* **Data Source :** [Wikepedia](https://en.wikipedia.org/wiki/List_of_S%26P_500_companies)      [Yahoo Finance](https://in.finance.yahoo.com/)         [finviz](https://finviz.com)
+""")
+
 container = st.beta_container()
 col1, col2 = container.beta_columns([5,5])
 st.sidebar.header("User Input Features")
@@ -38,7 +49,6 @@ sorted_sector_unique = sorted(df['GICS Sector'].unique())
 selected_sectors = st.sidebar.multiselect('Sector',sorted_sector_unique)
 df_selected_sectors = df[df['GICS Sector'].isin(selected_sectors)]
 st.sidebar.write(f"Companies : {df_selected_sectors.shape[0] or 0}")
-col1.title("Stock Analysis and Prediction")
 START =st.sidebar.date_input(label="Enter Start Date",value=date(2012,1,1))
 TODAY = date.today().strftime("%Y-%m-%d")
 if len(selected_sectors) != 0:
@@ -54,12 +64,13 @@ if selected_stock != None:
     data_load_state.text("Data Successfully Loaded")
     col1.subheader(f"{selected_stock} Raw Data")
     col1.write(data.tail(50))
+    traverser.traverse(selected_stock)
     def filedownload(df):
         csv = data.to_csv(index=False)
         b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
         href = f'<a href="data:file/csv;base64,{b64}" download="{selected_stock}.csv">Download CSV File</a>'
         return href
-    st.markdown(filedownload(df_selected_sectors), unsafe_allow_html=True)
+    col1.markdown(filedownload(df_selected_sectors), unsafe_allow_html=True)
 
 if data.empty == False:
     def plot_raw_data():
--- traverse.py
+++ traverse.py
@@ -1,16 +1,48 @@
 import os
 import os.path
 import time
+from datetime import datetime
 
-path = os.walk("models")
-model_files = []
-for root, directories, files in path:
-    model_files = files
-times = {}
-for file in model_files:
-    path_ = os.path.join(os.getcwd(),"models",file)
-    times[file] = os.path.getctime(path_)
-oldest_time = min(times.values())
-for file in times.keys():
-    if times[file] == oldest_time:
-        os.remove(os.path.join(os.getcwd(),"models",file))
+class Traverse:
+    def __init__(self):
+        self.path = os.walk("models")
+        self.model_files = []
+        self.init_files()
+        self.limit = 10
+    
+    def init_files(self):
+        for root, directories, files in self.path:
+            self.model_files = files
+    
+    def is_file_in_directory(self,ticker):
+        self.init_files()
+        if f"{ticker}.h5" in self.model_files:
+            return True
+        else:
+            return False
+
+    def remove_files(self):
+        times = {}
+        for file in self.model_files:
+            path_ = os.path.join(os.getcwd(),"models",file)
+            times[file] = os.path.getctime(path_)
+        oldest_time = min(times.values())
+        for file in times.keys():
+            if times[file] == oldest_time:
+                os.remove(os.path.join(os.getcwd(),"models",file))
+                break
+
+    def traverse(self,ticker):
+        self.init_files()
+        if self.is_file_in_directory(ticker):
+            path_ = os.path.join(os.getcwd(),"models",f"{ticker}.h5")
+            file_time = os.path.getctime(path_)
+            file_time = int(datetime.fromtimestamp(file_time).strftime('%d'))
+            now_time = int(datetime.now().strftime("%d"))
+            if file_time < now_time:
+                os.remove(os.path.join(os.getcwd(),"models",f"{ticker}.h5"))
+        else:
+            if len(self.model_files) < self.limit:
+                return
+            else:
+                self.remove_files()
